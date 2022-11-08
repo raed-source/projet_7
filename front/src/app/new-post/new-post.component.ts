98abab2ff603f350c5c0143350043101124506f0
raed ABBAS <raed.abbas@hotmail.fr>
2022-11-08 23:29:27 +0100

    creer premier post et deuxiem post

--- front/src/app/new-post/new-post.component.ts
+++ front/src/app/new-post/new-post.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { Post } from '../models/postModel';
+import { PostService } from '../services/postService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-post',
@@ -8,18 +12,30 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class NewPostComponent implements OnInit {
 postForm!:FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+postPreview$!: Observable<Post>;
+urlRegex!: RegExp;
+  constructor(private formBuilder:FormBuilder, private postService:PostService,private router:Router) { }
 
   ngOnInit(): void {
+    this.urlRegex=/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
     this.postForm=this.formBuilder.group({
-      title : [null],
-      content : [null],
-      imgUrl : [null],
-      dateCreate : [null],
-      location:[null]
-    })
+      title : [null,Validators.required],
+      content : [null,Validators.required],
+      imgUrl : [null,[Validators.required, Validators.pattern(this.urlRegex)]],
+      location:[null],
+    }, {
+      updateOn: 'blur'
+  });
+    this.postPreview$=this.postForm.valueChanges.pipe(map(formValue=>({
+      ...formValue,
+      createDate:new Date(),
+      id:0,
+      post:0
+    })))
   }
 onSubmitForm():void{
-  console.log(this.postForm.value)
+  console.log(this.postForm.value);
+  this.postService.addPost(this.postForm.value);
+  this.router.navigateByUrl('/posts');
 }
 }
