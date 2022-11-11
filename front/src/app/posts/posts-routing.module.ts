099ba0800d494da34e1068e8c60c543d414550d3
raed ABBAS <raed.abbas@hotmail.fr>
2022-11-11 10:07:25 +0100

    fini guards

--- front/src/app/posts/posts-routing.module.ts
+++ front/src/app/posts/posts-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { SinglePostComponent } from './components/single-post/single-post.component';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { NewPostComponent } from './components/new-post/new-post.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'create', component: NewPostComponent },
-  { path: ':id', component: SinglePostComponent },
-  { path: '', component: PostsListComponent },
+  { path: 'create', component: NewPostComponent ,canActivate:[AuthGuard]},
+  { path: ':id', component: SinglePostComponent,canActivate:[AuthGuard] },
+  { path: '', component: PostsListComponent,canActivate:[AuthGuard] },
 
 ];
 
